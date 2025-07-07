de65d31904661ed2b5608cb475a9b34ba005c3b7
Justin <457299@qq.com>
2025-07-07 22:03:11 +0800

    DRY scenes

--- scripts/piece_factory.gd
+++ scripts/piece_factory.gd
@@ -1,35 +1,60 @@
 class_name PieceFactory
 extends RefCounted
 
-const RED_PIECE_SECNES: Dictionary = {
-	"K": preload("res://scenes/red_king_piece.tscn"),
-	"A": preload("res://scenes/red_advisor_piece.tscn"),
-	"E": preload("res://scenes/red_elephant_piece.tscn"),
-	#"N",
-	"R": preload("res://scenes/red_rook_piece.tscn"),
-	#"C",
-	"P": preload("res://scenes/red_pawn_piece.tscn"),
+const PIECE_SCENE: PackedScene = preload("res://scenes/piece.tscn")
+
+const TYPES: Dictionary[String, Piece.TYPE] = {
+	"K": Piece.TYPE.KING,
+	"A": Piece.TYPE.ADVISOR,
+	"E": Piece.TYPE.ELEPHANT,
+	"N": Piece.TYPE.KNIGHT,
+	"R": Piece.TYPE.ROOK,
+	"C": Piece.TYPE.CANNON,
+	"P": Piece.TYPE.PAWN,
 }
 
-const BLACK_PIECE_SECNES: Dictionary = {
-	"K": preload("res://scenes/black_king_piece.tscn"),
-	"A": preload("res://scenes/black_advisor_piece.tscn"),
-	"E": preload("res://scenes/black_elephant_piece.tscn"),
-	#"N",
-	"R": preload("res://scenes/black_rook_piece.tscn"),
-	#"C",
-	"P": preload("res://scenes/black_pawn_piece.tscn"),
+const RED_REGION_RECTS: Dictionary[String, Rect2] = {
+	"K": Rect2(650, 144, 220, 220),
+	"A": Rect2(652, 387, 219, 219),
+	"E": Rect2(918, 391, 220, 219),
+	"N": Rect2(920, 635, 220, 220),
+	"R": Rect2(654, 633, 219, 220),
+	"C": Rect2(656, 902, 220, 219),
+	"P": Rect2(922, 904, 220, 220),
+}
+
+const BLACK_REGION_RECTS: Dictionary[String, Rect2] = {
+	"K": Rect2(394, 142, 220, 219),
+	"A": Rect2(396, 387, 220, 219),
+	"E": Rect2(142, 389, 220, 219),
+	"N": Rect2(144, 652, 220, 220),
+	"R": Rect2(397, 650, 220, 220),
+	"C": Rect2(398, 908, 220, 220),
+	"P": Rect2(144, 906, 220, 220),
+}
+
+const SCRIPTS: Dictionary[String, GDScript] = {
+	"K": preload("res://scripts/king_piece.gd"),
+	"A": preload("res://scripts/advisor_piece.gd"),
+	"E": preload("res://scripts/elephant_piece.gd"),
+	"N": null,
+	"R": preload("res://scripts/rook_piece.gd"),
+	"C": null,
+	"P": preload("res://scripts/pawn_piece.gd"),
 }
 
 static func build(color: Piece.COLOR, symbol: String) -> Piece:
 	assert(symbol.length() == 3, "symbol's length must be 3.")
-	var piece:Piece
+	var s := symbol[0]
+	var piece: Piece = PIECE_SCENE.instantiate()
+	piece.set_script(SCRIPTS[s])
+	piece.color = color
+	piece.type = TYPES[s]
 	if color == Piece.COLOR.RED:
-		piece = RED_PIECE_SECNES[symbol[0]].instantiate()
+		piece.region_rect = RED_REGION_RECTS[s]
 	else:
-		piece = BLACK_PIECE_SECNES[symbol[0]].instantiate()
-	var coordinate: Vector2i = Vector2i(\
-	symbol.unicode_at(1) - "a".unicode_at(0), \
-	symbol.unicode_at(2) - "0".unicode_at(0))
-	piece.coordinate = coordinate
+		piece.region_rect = BLACK_REGION_RECTS[s]
+	var x := symbol.unicode_at(1) - "a".unicode_at(0)
+	var y := symbol.unicode_at(2) - "0".unicode_at(0)
+	piece.coordinate = Vector2i(x, y)
 	return piece
